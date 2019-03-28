9e3c49052fb8fca8c340a9ba08abfd3dac606cc5
Ryan P Kilby <kilbyr@gmail.com>
2019-03-28 11:24:23 -0700

    Revert "support tox 3.8.0 (#30)"
    This reverts commit 70fd6b5690f51bd35ea454688cdb0ad50f6864ea.

--- setup.py
+++ setup.py
@@ -1,41 +1,41 @@
 import io
 
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 
 def get_long_description():
-    with io.open("README.rst", encoding="utf-8") as f:
+    with io.open('README.rst', encoding='utf-8') as f:
         return f.read()
 
 
 setup(
-    name="tox-venv",
-    description="Use python3 venvs for python3 tox testenvs",
+    name='tox-venv',
+    description='Use python3 venvs for python3 tox testenvs',
     long_description=get_long_description(),
-    author="Ryan P Kilby",
-    author_email="rpkilby@ncsu.edu",
-    url="https://github.com/tox-dev/tox-venv",
-    license="BSD",
-    version="0.3.1",
-    package_dir={"": "src"},
-    packages=find_packages("src"),
-    entry_points={"tox": ["venv = tox_venv.hooks"]},
-    install_requires=["tox >= 3.8.0, <4"],
+    author='Ryan P Kilby',
+    author_email='rpkilby@ncsu.edu',
+    url='https://github.com/tox-dev/tox-venv',
+    license='BSD',
+    version='0.3.1',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    entry_points={'tox': ['venv = tox_venv.hooks']},
+    install_requires=['tox>=2.0'],
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Framework :: tox",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Software Development :: Testing",
+        'Development Status :: 3 - Alpha',
+        'Framework :: tox',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Testing',
     ],
 )
--- src/tox_venv/hooks.py
+++ src/tox_venv/hooks.py
@@ -1,11 +1,10 @@
 import os
+import subprocess
 
-from tox import hookimpl, reporter
-from tox.exception import InvocationError
-from tox.venv import cleanup_for_venv
+import tox
 
 
-def real_python3(python, version_dict, action):
+def real_python3(python, version_dict):
     """
     Determine the path of the real python executable, which is then used for
     venv creation. This is necessary, because an active virtualenv environment
@@ -32,30 +31,27 @@ def real_python3(python, version_dict, action):
     do not contain an executable named `python`, so we attempt to derive this
     from the version info. e.g., `python3.6.5`, `python3.6`, then `python3`.
     """
-    args = [python, "-c", "import sys; print(sys.real_prefix)"]
-    popen_args = {"redirect": False, "returnout": True}
+    args = [python, '-c', 'import sys; print(sys.real_prefix)']
+
     # get python prefix
     try:
-        result = action.popen(args, report_fail=False, **popen_args)
-        prefix = result.strip()
-    except InvocationError:
+        output = subprocess.check_output(args, stderr=subprocess.STDOUT)
+        prefix = output.decode('UTF-8').strip()
+    except subprocess.CalledProcessError:
         # process fails, implies *not* in active virtualenv
         return python
 
     # determine absolute binary path
-    if os.name == "nt":  # pragma: no cover
+    if os.name == 'nt':  # pragma: no cover
         paths = [os.path.join(prefix, os.path.basename(python))]
     else:
-        paths = [
-            os.path.join(prefix, "bin", python)
-            for python in [
-                os.path.basename(python),
-                "python{major:d}.{minor:d}.{micro:d}".format(**version_dict),
-                "python{major:d}.{minor:d}".format(**version_dict),
-                "python{major:d}".format(**version_dict),
-                "python",
-            ]
-        ]
+        paths = [os.path.join(prefix, 'bin', python) for python in [
+            os.path.basename(python),
+            'python%(major)d.%(minor)d.%(micro)d' % version_dict,
+            'python%(major)d.%(minor)d' % version_dict,
+            'python%(major)d' % version_dict,
+            'python',
+        ]]
 
     for path in paths:
         if os.path.isfile(path):
@@ -64,10 +60,10 @@ def real_python3(python, version_dict, action):
         path = None
 
     # the executable path must exist
-    assert path, "\n- ".join(["Could not find interpreter. Attempted:"] + paths)
-    v1 = action.popen([python, "--version"], **popen_args)
-    v2 = action.popen([path, "--version"], **popen_args)
-    assert v1 == v2, "Expected versions to match ({} != {}).".format(v1, v2)
+    assert path, '\n- '.join(['Could not find interpreter. Attempted:'] + paths)
+    v1 = subprocess.check_output([python, '--version'])
+    v2 = subprocess.check_output([path, '--version'])
+    assert v1 == v2, 'Expected versions to match (%s != %s).' % (v1, v2)
 
     return path
 
@@ -82,35 +78,28 @@ def use_builtin_venv(venv):
     return version is not None and version >= (3, 3)
 
 
-@hookimpl
+@tox.hookimpl
 def tox_testenv_create(venv, action):
     # Bypass hook when venv is not available for the target python version
     if not use_builtin_venv(venv):
         return
 
     v = venv.envconfig.python_info.version_info
-    version_dict = {"major": v[0], "minor": v[1], "micro": v[2]}
+    version_dict = {'major': v[0], 'minor': v[1], 'micro': v[2]}
 
     config_interpreter = str(venv.getsupportedinterpreter())
-    real_executable = real_python3(config_interpreter, version_dict, action)
+    real_executable = real_python3(config_interpreter, version_dict)
 
-    args = [real_executable, "-m", "venv"]
+    args = [real_executable, '-m', 'venv']
     if venv.envconfig.sitepackages:
-        args.append("--system-site-packages")
+        args.append('--system-site-packages')
     if venv.envconfig.alwayscopy:
-        args.append("--copies")
+        args.append('--copies')
 
-    cleanup_for_venv(venv)
-
+    venv.session.make_emptydir(venv.path)
     basepath = venv.path.dirpath()
     basepath.ensure(dir=1)
     args.append(venv.path.basename)
-    venv._pcall(
-        args,
-        venv=False,
-        action=action,
-        cwd=basepath,
-        redirect=reporter.verbosity() < reporter.Verbosity.DEBUG,
-    )
+    venv._pcall(args, venv=False, action=action, cwd=basepath)
     # Return non-None to indicate the plugin has completed
     return True
